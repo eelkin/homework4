cca42692b18ef027f10888048418d48e7948d17b
Evan Elkin <eelkin@carpenter-04.elon.edu>
2016-12-04 19:26:00 -0500

    added java files for data and model, added copyright statements to java files

---
+++ src/java/data/ConnectionPool.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2016 Evan Elkin
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ * 
+ * Copyright (c) 2016 Breslin Wiley
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ */
+package data;
+
+public class ConnectionPool {
+  
+}
---
+++ src/java/model/User.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2016 Evan Elkin
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ * 
+ * Copyright (c) 2016 Breslin Wiley
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ */
+package model;
+
+/**
+ *
+ * @author eelkin
+ */
+public class User {
+  
+}
