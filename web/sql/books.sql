93f55f5d2fbd2be64749a299cb0b13ad3c619610
Evan Elkin <eelkin@mooney202-01.elon.edu>
2016-12-05 11:29:12 -0500

    added empty sql file

---
+++ web/sql/books.sql
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2016 Evan Elkin
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ * 
+ * Copyright (c) 2016 Breslin Wiley
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ */
+
