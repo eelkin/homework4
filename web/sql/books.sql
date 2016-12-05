e80c62f9f88f33e4617cf1d40e15646f19b2c6d8
Evan Elkin <eelkin@mooney202-01.elon.edu>
2016-12-05 11:36:51 -0500

    filled in SQL file

--- web/sql/books.sql
+++ web/sql/books.sql
@@ -6,3 +6,32 @@
  * Campus Box 7947, Elon University, Elon, NC 27244
  */
 
+CREATE DATABASE IF NOT EXISTS nightsound;
+USE nightsound;
+
+DROP TABLE IF EXISTS Books;
+
+CREATE TABLE Books (
+  UserID INT NOT NULL AUTO_INCREMENT,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Email VARCHAR(50),
+  BookTitle NOT NULL VARCHAR(50),
+  DueDate VARCHAR(50),
+  Overdue VARCHAR(50)
+  
+  PRIMARY KEY(BookTitle) 
+);
+
+/*
+USE CODE BELOW FOR TESTING PURPOSES
+*/
+
+/*
+INSERT INTO User 
+  (Email, FirstName, LastName)
+VALUES 
+  ('jsmith@gmail.com', 'John', 'Smith'),
+  ('andi@murach.com', 'Andrea', 'Steelman'), 
+  ('joelmurach@yahoo.com', 'Joel', 'Murach');
+*/
