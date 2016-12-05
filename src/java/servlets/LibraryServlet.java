11595c54c75e95054d778402dcd2e6058aa8bf4a
Evan Elkin <eelkin@carpenter-04.elon.edu>
2016-12-04 19:44:15 -0500

    filled in code for servlet and data classes, need to edit them for database

--- src/java/servlets/LibraryServlet.java
+++ src/java/servlets/LibraryServlet.java
@@ -1,86 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2016 Evan Elkin
+ * Campus Box 7947, Elon University, Elon, NC 27244
+ * 
+ * Copyright (c) 2016 Breslin Wiley
+ * Campus Box 7947, Elon University, Elon, NC 27244
  */
 package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author eelkin
- */
+import model.User;
+import data.UserDB;
+
 public class LibraryServlet extends HttpServlet {
 
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-      /* TODO output your page here. You may use following sample code. */
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet LibraryServlet</title>");      
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Servlet LibraryServlet at " + request.getContextPath() + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+
+        String url = "/index.jsp";
+        
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "display_users";
+        }
+        
+        // perform action and set URL to appropriate page
+        if (action.equals("display_users")) {            
+            // get list of users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);
+        } 
+        else if (action.equals("display_user")) {
+            String emailAddress = request.getParameter("email");
+            User user = UserDB.selectUser(emailAddress);
+            session.setAttribute("user", user);
+            url = "/user.jsp";
+        }
+        else if (action.equals("update_user")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+
+            // get and update user
+            User user = (User) session.getAttribute("user");        
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            UserDB.update(user);
+
+            // get and set updated users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);            
+        }
+        else if (action.equals("delete_user")) {
+            // get the user
+            String email = request.getParameter("email");
+            User user = UserDB.selectUser(email);
+            
+            // delte the user
+            UserDB.delete(user);
+            
+            // get and set updated users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);            
+        }
+        
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }    
+    
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
     }
-  }
-
-  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-  /**
-   * Handles the HTTP <code>GET</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
-
-  /**
-   * Handles the HTTP <code>POST</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
-
-  /**
-   * Returns a short description of the servlet.
-   *
-   * @return a String containing servlet description
-   */
-  @Override
-  public String getServletInfo() {
-    return "Short description";
-  }// </editor-fold>
 
 }
